21ad5f7a668c5a5a72a85f8ca739a2c21ae20e0f
James Grieve <jamesonrgrieve@gmail.com>
2020-10-21 09:41:24 -0600

    In-class practice;

--- CSharpOOP/CSharpOOP/Person.cs
+++ CSharpOOP/CSharpOOP/Person.cs
@@ -6,10 +6,19 @@ namespace CSharpOOP
 {
     class Person
     {
+        // In-Class Practice:
+        // Add an "Age" property. Validate that the incoming "value" is greater than or equal to zero before setting the backing variable.
+        // If it is not, throw an Exception.
+
+
         // Public items can be seen anywhere.
         // Auto-Implemented Property (Automatically generates a default "getter" (accessor) and "setter" (mutator).
         // AccessModifier DataType PropertyName { get; set; }
         public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        /*
 
         // Private items cannot be seen outside of the class. Marking it as private enforces that all data to be stored MUST go through the property.
         private string _lastName;
@@ -28,5 +37,7 @@ namespace CSharpOOP
                 _lastName = value;
             } 
         }
+
+        */
     }
 }
