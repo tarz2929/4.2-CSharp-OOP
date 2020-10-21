e0a8cd6c426029e04a3f285fcfc1ed2568612488
James Grieve <jamesonrgrieve@gmail.com>
2020-10-21 09:27:28 -0600

    Initial commit; First class and properties;

---
+++ CSharpOOP/CSharpOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharpOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName;
+
+            Console.Write("Please enter your first name: ");
+            firstName = Console.ReadLine().Trim();
+
+            Console.Write("Please enter your last name: ");
+            lastName = Console.ReadLine().Trim();
+
+            Console.WriteLine($"Hello, {firstName} {lastName}!");
+        }
+    }
+}
