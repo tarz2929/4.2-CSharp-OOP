6b64078c55fb2c1fde912570b0ddfc49dd8bbc49
James Grieve <jamesonrgrieve@gmail.com>
2020-10-22 09:56:02 -0600

    Property validation;

--- CSharpOOP/CSharpOOP/Student.cs
+++ CSharpOOP/CSharpOOP/Student.cs
@@ -9,8 +9,37 @@ namespace CSharpOOP
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public int EnergyLevel { get; set; }
-        public int StressLevel { get; set; }
+        private int _energyLevel;
+        public int EnergyLevel
+        {
+            get
+            {
+                return _energyLevel;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Insufficient energy to perform that action.");
+                }
+                _energyLevel = value;
+            }
+        }
+        private int _stressLevel;
+        public int StressLevel {
+            get
+            {
+                return _stressLevel;
+            }
+            set
+            {
+                if (value > 100)
+                {
+                    throw new Exception("Too much stress to perform that action.");
+                }
+                _stressLevel = value;
+            }
+        }
 
         public void DoHomework()
         {
@@ -18,7 +47,7 @@ namespace CSharpOOP
             StressLevel += 25;
         }
 
-        public Student (string firstName, string lastName)
+        public Student(string firstName, string lastName)
         {
             FirstName = firstName;
             LastName = lastName;
