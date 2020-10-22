d8cae5b60d664017fc121419076f9bea24480f6e
James Grieve <jamesonrgrieve@gmail.com>
2020-10-22 09:52:30 -0600

    Initial class setup;

---
+++ CSharpOOP/CSharpOOP/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpOOP
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int EnergyLevel { get; set; }
+        public int StressLevel { get; set; }
+
+        public void DoHomework()
+        {
+            EnergyLevel -= 20;
+            StressLevel += 25;
+        }
+
+        public Student (string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            EnergyLevel = 100;
+            StressLevel = 0;
+        }
+    }
+}
---
+++ CSharpOOP/CSharpOOP/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpOOP
+{
+    class Teacher
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public List<Student> Class { get; set; }
+
+        public Teacher(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+
+            Class = new List<Student>();
+        }
+    }
+}
