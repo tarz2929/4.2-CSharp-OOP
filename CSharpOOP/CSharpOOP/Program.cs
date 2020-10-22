7ae388f3276f556f07457b243e6c5a5218f1d057
James Grieve <jamesonrgrieve@gmail.com>
2020-10-22 10:04:45 -0600

    Quick object reference demo;

--- CSharpOOP/CSharpOOP/Program.cs
+++ CSharpOOP/CSharpOOP/Program.cs
@@ -6,7 +6,26 @@ namespace CSharpOOP
     {
         static void Main(string[] args)
         {
-           // Starting point for in-class demos.
+            Teacher mathTeacher = new Teacher("Bob", "Jones");
+
+            Student joe = new Student("Joe", "Smith");
+            Student sally = new Student("Sally", "Howard");
+
+            mathTeacher.Class.Add(joe);
+            mathTeacher.Class.Add(sally);
+
+            Console.WriteLine($"joe variable: {joe.FirstName} {joe.LastName}");
+            Console.WriteLine($"First Student: {mathTeacher.Class[0].FirstName} {mathTeacher.Class[0].LastName}");
+
+            joe.LastName = "Johnson";
+
+            Console.WriteLine($"joe variable: {joe.FirstName} {joe.LastName}");
+            Console.WriteLine($"First Student: {mathTeacher.Class[0].FirstName} {mathTeacher.Class[0].LastName}");
+
+            joe = new Student("Joe", "Smith");
+
+            Console.WriteLine($"joe variable: {joe.FirstName} {joe.LastName}");
+            Console.WriteLine($"First Student: {mathTeacher.Class[0].FirstName} {mathTeacher.Class[0].LastName}");
         }
     }
 }
